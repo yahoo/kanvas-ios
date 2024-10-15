39c8f7fc3facced8e3571d5c174df2d0730cc404
Jose Solorzano <jose.solorzano@yahooinc.com>
2024-10-15 14:34:18 -0700

    Renames resources folder

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
             name: "Kanvas",
             dependencies: [.product(name: "CropViewController", package: "TOCropViewController")],
             path: "Classes",
-            resources: [.copy("Resources")]
+            resources: [.copy("Files")]
         ),
     ]
 )
